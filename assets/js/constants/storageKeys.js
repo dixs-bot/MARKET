84ddaed0e23f492d70c2c90234e7a8069cf9a6c3
dikiwahyudin <74003372+dixs-bot@users.noreply.github.com>
2026-05-11 04:30:33 +0700

    Update storageKeys.js

--- assets/js/constants/storageKeys.js
+++ assets/js/constants/storageKeys.js
@@ -1,13 +1,21 @@
-export function getCartKey(storeId) {
+window.getCartKey = function(storeId) {
+
   return `lumora_cart_${storeId}`;
-}
+};
 
-export function getOrderKey(storeId) {
+window.getOrderKey = function(storeId) {
+
   return `lumora_orders_${storeId}`;
-}
+};
 
-export const STORAGE_KEYS = {
-  CUSTOMER_STORE: 'lumora_selected_store',
-  CUSTOMER_SESSION: 'lumora_customer_session',
-  ADMIN_SESSION: 'lumora_admin_session'
+window.STORAGE_KEYS = {
+
+  CUSTOMER_STORE:
+    'lumora_selected_store',
+
+  CUSTOMER_SESSION:
+    'lumora_customer_session',
+
+  ADMIN_SESSION:
+    'lumora_admin_session'
 };
