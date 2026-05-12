afdc8b03f059ea7acf2fe3fdedf84e0340927d1d
dikiwahyudin <74003372+dixs-bot@users.noreply.github.com>
2026-05-13 04:50:56 +0700

    Create services.js

---
+++ assets/js/services.js
@@ -0,0 +1,7 @@
+export * from './services/cart.service.js';
+
+export * from './services/checkout.service.js';
+
+export * from './services/validation.service.js';
+
+export * from './services/stores.service.js';
