b1ed484ad3da4568a433a13fe36b3315b72bfaf4
dikiwahyudin <74003372+dixs-bot@users.noreply.github.com>
2026-05-13 04:50:11 +0700

    Update index.js

--- assets/js/ui/index.js
+++ assets/js/ui/index.js
@@ -1,12 +1,12 @@
 
 
-export { cache, animateIn } from './ui/animation.js';
+export { cache, animateIn } from './animation.js';
 
-export { lock, unlock } from './ui/loading.js';
+export { lock, unlock } from './loading.js';
 
-export { notify } from './ui/toast.js';
+export { notify } from './toast.js';
 
-export { openVou, closeVou } from './ui/modal.js';
+export { openVou, closeVou } from './modal.js';
 
 export {
     qtyHTML,
@@ -18,15 +18,15 @@ export {
     renderCart,
     openCart,
     closeCart
-} from './ui/cart.ui.js';
+} from './cart.ui.js';
 
 export {
     renderShips,
     renderPays,
     renderSummary
-} from './ui/checkout.ui.js';
+} from './checkout.ui.js';
 
 export {
     renderInv,
     renderOrders
-} from './ui/invoice.ui.js';
+} from './invoice.ui.js';
